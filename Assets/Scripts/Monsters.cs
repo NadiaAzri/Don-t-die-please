e2255a2afad1ec6b680b2c95f6d994b327806288
NadiaAzri <azrinadia79@gmail.com>
2021-12-29 15:56:04 +0100

    a simple 2D game using c# and unity3D

---
+++ Assets/Scripts/Monsters.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monsters : MonoBehaviour {
+
+	[HideInInspector]
+	public float speed;
+
+	private Rigidbody2D myBody;
+
+	// Use this for initialization
+	void Awake () {
+
+		myBody = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		myBody.velocity = new Vector2(speed, myBody.velocity.y);
+		
+	}
+}
